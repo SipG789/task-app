f99c6e7cca98b2da0f678c98933c35df2c4b4c1d
Sip <sipg789@gmail.com>
2022-11-15 15:15:34 -0600

    refactor code

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,18 +8,30 @@ var createTaskHandler = function(event) {
 
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
 
+    // send it as an argument 
+    createTaskEl(taskDataObj);
 
+};
+
+var createTaskEl = function(taskDataObj) {
+        // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
 
+    // create div to hold task info and add to list item
     var taskInfoEl = document.createElement("div");
-        taskInfoEl.className= "task-info";
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
 
-    
-    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class = 'task-type'>" + taskTypeInput + "</span>";
     listItemEl.appendChild(taskInfoEl);
 
+    // add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 };
 
